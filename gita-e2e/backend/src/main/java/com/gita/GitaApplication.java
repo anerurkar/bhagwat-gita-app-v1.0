34a5030e7483725a98f9752beb010d0e5995b4d7
anand nerurkar <anand.nerurkar@gmail.com>
2026-01-17 19:04:01 +0530

    mobileapp firebase host cors added

--- gita-e2e/backend/src/main/java/com/gita/GitaApplication.java
+++ gita-e2e/backend/src/main/java/com/gita/GitaApplication.java
@@ -44,6 +44,7 @@ public class GitaApplication {
                             "https://kshna-svc-100157816972.asia-south1.run.app",
 							"https://*.web.app",
 							"https://gita-app-483814.web.app",
+							"https://gita-app-mobile.web.app",
                             "https://*.firebaseapp.com"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
